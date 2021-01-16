ee8ce7452b5b825472f7528bb8f5478c023a037b
Manuel Alberto <manuel.ortiz.is@unipoli.edu.mx>
2021-01-15 19:02:32 -0600

    Modelo de un sitio web

---
+++ scritps/script-menu.js
@@ -0,0 +1,18 @@
+jQuery('document').ready(function($){
+    
+    var menuBtn = $('.menu-icon'),
+        menu = $('nav');
+        menuLetras = $('.list ul');
+    
+    menuBtn.click(function() {
+
+        if (menu.hasClass('show') && menuLetras.hasClass('show2')) {
+            menu.removeClass('show');
+            menuLetras.removeClass('show2');
+        } else {
+            menu.addClass('show');
+        menuLetras.addClass('show2');
+        }
+
+    });
+});
